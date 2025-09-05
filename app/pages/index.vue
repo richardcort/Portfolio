42dc145446863c390d85f702fd1c813b7be8f769
richardcort <144558240+richardcort@users.noreply.github.com>
2025-09-05 15:40:24 -0400

    feat: add new components and styles

---
+++ app/pages/index.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import Card from '~/components/Card.vue';
+</script>
+
+<template>
+    <div class="flex flex-col items-center justify-center">
+        <Card title="Richard Cortez">
+            <template v-slot:content>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.
+                </p>
+            </template>
+        </Card>
+        <hr>
+        <Card title="Experience">
+            <template v-slot:content>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.
+                </p>
+            </template>
+        </Card>
+        <Card title="Projects">
+            <template v-slot:content>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.
+                </p>
+            </template>
+        </Card>
+        <Card title="Socials">
+            <template v-slot:content>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.
+                </p>
+            </template>
+        </Card>
+        <Card title="Email">
+            <template v-slot:content>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.
+                </p>
+            </template>
+        </Card>
+    </div>
+</template>
