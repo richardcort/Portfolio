160f97a50bb398d2b3672c23fc68e5380e691573
richard cortez <richardcortez934@gmail.com>
2025-09-05 15:40:24 -0400

    feat: add new components and styles

---
+++ app/components/Card.vue
@@ -0,0 +1,21 @@
+<template>
+    <section>
+        <header>
+            <h2 class="text-2xl font-bold">
+                {{ title }}
+            </h2>
+            <Icon class="size-8"/>
+        </header>
+        <div>
+            <slot name="content"></slot>
+        </div>
+    </section>
+</template>
+
+<script setup lang="ts">
+import Icon from './Icon.vue';
+
+defineProps<{
+    title: string;
+}>();
+</script>
