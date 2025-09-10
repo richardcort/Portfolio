f64cc315288071e1804b4395a371f0e10ef8ad68
richard cortez <richardcortez934@gmail.com>
2025-09-10 15:38:51 -0400

    feat: set conf

---
+++ app/components/Section.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="w-full">
+        <header class="font-bold text-2xl text-gray-100 mb-6 flex items-center gap-3">
+            <h2>
+                {{ title }}
+            </h2>
+            <slot name="icon" />
+        </header>
+        <slot name="content" />
+    </section>
+</template>
+
+<script setup lang="ts">
+
+defineProps<{
+    title: string;
+    contentClass?: string;
+}>();
+
+
+</script>
