2e69714f3f63cb0ef88162ff4e43441c79b95cc6
richardcort <144558240+richardcort@users.noreply.github.com>
2025-09-10 15:38:51 -0400

    feat: set conf

---
+++ app/layouts/default.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+
+</script>
+<template>
+    <body class="bg-gray-800 pb-0 sm:pb-6 bg-opacity-25">
+        <slot />
+    </body>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,44 +1,53 @@
-<script setup lang="ts">
-import Card from '~/components/Card.vue';
-</script>
-
 <template>
-    <div class="flex flex-col items-center justify-center">
-        <Card title="Richard Cortez">
-            <template v-slot:content>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.
-                </p>
+    <!--que es max-w-3xl-->
+    <div class="mx-auto max-w-3xl px-5 mb-5 flex flex-col gap-10">
+        <Section title="Experience">
+            <template #icon>
+                <IconsBrief />
             </template>
-        </Card>
-        <hr>
-        <Card title="Experience">
-            <template v-slot:content>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.
-                </p>
+            <template #content>
+                <div class="flex flex-col gap-10">
+                    <div class="flex flex-col gap-5">
+                        <div class="flex flex-col gap-1">
+                            <h2 class="text-lg font-semibold text-slate-200">Backend Developer</h2>
+                        </div>
+                        <ul class="list-disc flex flex-col gap-2 ml-5 text-gray-300 text-base">
+                            <li>2021 - Present</li>
+                            <li>Remote</li>
+                            <li>Java, Spring Boot, Kotlin, PostgreSQL</li>
+                            <li>Worked on various projects including e-commerce platforms and internal tools.</li>
+                            <li>Collaborated with cross-functional teams to deliver high-quality software solutions.
+                            </li>
+                            <li>Implemented RESTful APIs and optimized database queries for performance.</li>
+                        </ul>
+                    </div>
+                </div>
             </template>
-        </Card>
-        <Card title="Projects">
-            <template v-slot:content>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.
-                </p>
+        </Section>
+        <Section title="Projects">
+            <template #icon>
+                <IconsStar />
             </template>
-        </Card>
-        <Card title="Socials">
-            <template v-slot:content>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.
-                </p>
+            <template #content>
             </template>
-        </Card>
-        <Card title="Email">
-            <template v-slot:content>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quia.
-                </p>
+        </Section>
+        <Section title="Socials">
+            <template #icon>
+                <IconsAlternate />
             </template>
-        </Card>
+            <template #content>
+            </template>
+        </Section>
+        <Section title="Email">s
+            <template #icon>
+                <IconsEmail />
+            </template>
+            <template #content>
+            </template>
+        </Section>
     </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
