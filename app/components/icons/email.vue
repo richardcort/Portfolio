6d5738b388f5a8bb811a17669137faa0e7c33e70
richard cortez <richardcortez934@gmail.com>
2025-09-10 14:53:35 -0400

    feat: add new icon components

---
+++ app/components/icons/email.vue
@@ -0,0 +1,8 @@
+<template>
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32"
+        viewBox="0 0 14 14"><!-- Icon from Streamline by Streamline - https://creativecommons.org/licenses/by/4.0/ -->
+        <path fill="currentColor" fill-rule="evenodd"
+            d="M0 2.75a1.5 1.5 0 0 1 1.5-1.5h11a1.5 1.5 0 0 1 1.5 1.5v.342L7.383 7.504A.7.7 0 0 1 7 7.607a.7.7 0 0 1-.383-.103L0 3.092zm0 1.844v6.656a1.5 1.5 0 0 0 1.5 1.5h11a1.5 1.5 0 0 0 1.5-1.5V4.594L8.073 8.546l-.005.004A1.97 1.97 0 0 1 7 8.857c-.375 0-.753-.102-1.068-.307l-.005-.004z"
+            clip-rule="evenodd" />
+    </svg>
+</template>
