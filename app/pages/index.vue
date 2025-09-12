d2309ccda21383808c5cacd04f910affe1323297
richard cortez <richardcortez934@gmail.com>
2025-09-12 15:33:11 -0400

    feat: changes

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -9,16 +9,10 @@
                 <div class="flex flex-col gap-10">
                     <div class="flex flex-col gap-5">
                         <div class="flex flex-col gap-1">
-                            <h2 class="text-lg font-semibold text-slate-200">Backend Developer</h2>
+                            <h2 class="text-lg font-semibold text-slate-200">Jr Back-End Developer • Alcaldia de Iribarren</h2>
                         </div>
                         <ul class="list-disc flex flex-col gap-2 ml-5 text-gray-300 text-base">
-                            <li>2021 - Present</li>
-                            <li>Remote</li>
-                            <li>Java, Spring Boot, Kotlin, PostgreSQL</li>
-                            <li>Worked on various projects including e-commerce platforms and internal tools.</li>
-                            <li>Collaborated with cross-functional teams to deliver high-quality software solutions.
-                            </li>
-                            <li>Implemented RESTful APIs and optimized database queries for performance.</li>
+                            <li>January 2025 - Present</li>
                         </ul>
                     </div>
                 </div>
@@ -29,6 +23,9 @@
                 <IconsStar />
             </template>
             <template #content>
+                <div class="grid grid-cols-1 md:grid-cols-2 gap-2 sm:gap-5">
+                    <Card href="#"  title="Example" description="Example of a description"/>
+                </div>
             </template>
         </Section>
         <Section title="Socials">
@@ -36,18 +33,38 @@
                 <IconsAlternate />
             </template>
             <template #content>
+                <div class="flex flex-wrap gap-3">
+                    <Link href="https://github.com/richardcort" label="Github" target="_blank">
+                        <IconsGithub />
+                    </Link>
+                    <Link href="#" label="Linkedin" target="_blank" color="text-sky-600">
+                        <IconsLinkedin />
+                    </Link>
+                </div>
             </template>
         </Section>
-        <Section title="Email">s
+        <Section title="Email">
             <template #icon>
                 <IconsEmail />
             </template>
             <template #content>
+                <div class="flex gap-2">
+                    <input class="bg-white/5 w-full py-1.5 px-3 rounded-xl grid place-items-center text-slate-300"
+                        type="text" value="richardcortez934@gmail.com" disabled>
+                    <div class="flex gap-2">
+                        <ButtonTooltip label="Send email" tag="a" class="bg-indigo-500 py-2.5 px-4" href="mailto:richardcortez934@gmail.com">
+                            <IconsSend />
+                        </ButtonTooltip>
+                        <ButtonTooltip label="Copy email" tag="button" class="bg-indigo-500 py-2.5 px-4 ">
+                            <IconsCopy />
+                        </ButtonTooltip>
+                    </div>
+                </div>
             </template>
         </Section>
     </div>
 </template>
 
 <script setup lang="ts">
-
+import Link from '~/components/Link.vue';
 </script>
