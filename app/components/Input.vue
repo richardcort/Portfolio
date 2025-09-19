a58ca888e47ac8c43229145529f0ed3098713c40
richard cortez <richardcortez934@gmail.com>
2025-09-19 15:35:12 -0400

    feat: add Card component and Input component

---
+++ app/components/Input.vue
@@ -0,0 +1,14 @@
+<template>
+    <input class="bg-white/5 w-full py-1.5 px-3 rounded-xl grid place-items-center text-slate-300"
+        :type="props.type" 
+        :value="props.value" 
+        :disabled="props.disabled"
+    >
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+    value: string;
+    disabled?: boolean;
+    type?: string;
+}>();
+</script>
