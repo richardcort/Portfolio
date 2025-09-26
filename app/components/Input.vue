7a56c0b256b6b51a6831f411a59975182c082f38
richard cortez <richardcortez934@gmail.com>
2025-09-26 14:34:43 -0400

    feat: Refactor Input component to support multiple input types and improve code clarity

--- app/components/Input.vue
+++ app/components/Input.vue
@@ -1,14 +1,25 @@
 <template>
-    <input class="bg-white/5 w-full py-1.5 px-3 rounded-xl grid place-items-center text-slate-300"
-        :type="props.type" 
+    <input 
+        class="bg-white/5 w-full py-1.5 px-3 rounded-xl grid place-items-center text-slate-300"
+        :type="inputType"
         :value="props.value" 
         :disabled="props.disabled"
     >
 </template>
 <script setup lang="ts">
+
+enum InputType {
+    text = 'text',
+    password = 'password',
+    email = 'email',
+    number = 'number',
+}
+
 const props = defineProps<{
+    type?: InputType;
     value: string;
     disabled?: boolean;
-    type?: string;
 }>();
+
+const inputType = computed(() => props.type ?? InputType.text)
 </script>
